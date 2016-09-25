1bda94c0b46d84a795b80e434ac1fc365eac17c5
Chris Peleti <chrisyred@gmail.com>
2016-09-25 15:12:50 +0000

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.size==1
+    array.join
+  elsif array.size==2
+    array.join(" and ")
+  elsif array.size>=3
+    new_list=array[0..(array.size-2)].join(", ")
+    new_list=new_list + ", and #{array[-1]}"
+    return new_list
+
+
+    end
 
 end
